e0709d0df985b6188e7fed577b7e136a590e906d
Francisco Trindade <francisco.ad.trindade@gmail.com>
2018-12-03 15:40:34 -0500

    more boilerplate

--- make-day.sh
+++ make-day.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 DAY_DIR=day-${1}
+TEMPLATE=template.py
 
-mkdir ${DAY_DIR} 
-touch ${DAY_DIR}/part_1.py
-touch ${DAY_DIR}/part_2.py
-
+if [ -d ${DAY_DIR} ]; then
+    echo "${DAY_DIR} already exists!"
+else
+    mkdir ${DAY_DIR} 
+    cp ${TEMPLATE} ${DAY_DIR}/part_1.py
+    cp ${TEMPLATE} ${DAY_DIR}/part_2.py
+    touch ${DAY_DIR}/day_${1}.in
+fi
---
+++ template.py
@@ -0,0 +1,5 @@
+import sys
+
+for val in open(sys.argv[1]).readlines():
+    _ = val.rstrip()
+
