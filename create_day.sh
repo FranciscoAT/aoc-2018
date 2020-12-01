c7dda61296949cc55c99eb544fca0afc4459d398
Francisco Trindade <francisco.ad.trindade@gmail.com>
2020-12-01 17:46:36 -0500

    Rework create_day for 2020 and generate day1

--- create_day.sh
+++ create_day.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-dir_name=2019/day-${1}
+YEAR=2020
 
-mkdir -p ${dir_name}/src
-touch ${dir_name}/src/{part_1.py,part_2.py,day-${1}.in,part-1.out,part-2.out}
-touch ${dir_name}/README.md
+dir_name=${YEAR}/day-${1}
 
-echo "- [Day ${1}](./day-${1})" >> ./2019/README.md
-echo "# [Day ${1}](./src)" >> ./2019/day-${1}/README.md
-echo "### [Part one](./src/part_1.py)" >> ./2019/day-${1}/README.md
-echo "Runtime: xxx" >> ./2019/day-${1}/README.md
-echo "### [Part two](./src/part_2.py)" >> ./2019/day-${1}/README.md
-echo "Runtime: xxx" >> ./2019/day-${1}/README.md
+mkdir -p ${dir_name}
+touch ${dir_name}/{part_1.py,part_2.py,day-${1}.in}
